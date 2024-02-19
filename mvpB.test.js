a86e2679ba4cfa57bdf88b9ef74f8c1bd2a87a82
Natalie Thompson <nataliethompson2314@gmail.com>
2024-02-19 09:35:56 -0800

    pls work

--- mvpB.test.js
+++ mvpB.test.js
@@ -1,6 +1,6 @@
-import React from 'react'
-import { render, screen } from '@testing-library/react'
-import '@testing-library/jest-dom'
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
 
 describe('Sprint 7 Challenge Learner Tests', () => {
   /*
